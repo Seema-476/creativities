66beb8790c9333ef49c8124b32519669cc21d823
Seema-476 <simmybishnoi29085@gmail.com>
2025-01-04 20:53:19 +0530

    changes

--- src/components/home/ThemeChange.jsx
+++ src/components/home/ThemeChange.jsx
@@ -21,7 +21,7 @@ const ThemeChange = () => {
     <div className={isDarkMode ? 'dark' : ''}>
       <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-all">
         <header className="p-4 flex justify-between items-center">
-          <h1 className="text-2xl font-bold">Dark/Light Mode Toggle</h1>
+          <h1 className="text-2xl font-bold sm:block hidden">Dark/Light Mode Toggle</h1>
           <button
             onClick={toggleTheme}
             className="p-2 rounded-full bg-gray-200 dark:bg-gray-700 hover:scale-95 duration-700 text-black dark:text-white"
