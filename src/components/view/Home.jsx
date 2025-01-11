13b59c2a01d7e31bdae9364cfb006a61cd1413fc
Seema-476 <simmybishnoi29085@gmail.com>
2025-01-11 21:40:04 +0530

    mood tracker creativity done

--- src/components/view/Home.jsx
+++ src/components/view/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ThemeChange from '../home/ThemeChange'
+import MoodTracker from '../home/MoodTracker'
 
 const Home = () => {
     return (
-        <ThemeChange />
+        <>
+            <ThemeChange />
+            <MoodTracker />
+        </>
     )
 }
 
---
+++ src/utils/helper.js
@@ -0,0 +1,6 @@
+export const TRACKER_DATA = [
+    { name: "Happy", emoji: "😊", quote: "Happiness is a warm glow from within." },
+    { name: "Sad", emoji: "😢", quote: "Tears are words the heart can't express." },
+    { name: "Excited", emoji: "🤩", quote: "Excitement is the spark of life!" },
+    { name: "Calm", emoji: "😌", quote: "Peace begins with a smile." },
+];
