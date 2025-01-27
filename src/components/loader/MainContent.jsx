3d37c67ca7114204825f37a35e09ad2826084582
Seema-476 <simmybishnoi29085@gmail.com>
2025-01-27 09:15:28 +0530

    changes

--- src/components/loader/MainContent.jsx
+++ src/components/loader/MainContent.jsx
@@ -5,9 +5,9 @@ const MainContent = () => {
     return (
         <>
             <MainPage />
-            <div className='flex items-center justify-center h-screen w-full bg-slate-500 duration-500'>
-                <p className='text-7xl font-bold text-white'>WELCOME!</p>
-            </div>
+            <div className='flex items-center justify-center w-full h-screen bg-slate-500 duration-500'>
+                    <p className='text-7xl font-bold text-white'>Loader with Gsap</p>
+                </div>
         </>
   )
 }
