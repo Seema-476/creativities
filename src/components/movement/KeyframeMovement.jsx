f66cb0d6aa33001bd155968380a86c3f09520f6f
Seema-476 <simmybishnoi29085@gmail.com>
2025-02-03 15:20:53 +0530

    changes

--- src/components/movement/KeyframeMovement.jsx
+++ src/components/movement/KeyframeMovement.jsx
@@ -3,15 +3,15 @@ import { CARD_ANIMATION_DATA_LIST } from "../../utils/helper";
 
 const KeyframeMovement = () => {
   return (
-    <div className="h-screen flex w-full">
+    <div className="h-screen flex w-full items-center justify-center">
       <div className="container mx-auto px-5">
-        <h1 className="lg:text-5xl text-2xl font-bold text-blue-300 py-10 text-center">
-          HOVER ON CARD
+        <h1 className="lg:text-5xl text-2xl font-bold text-blue-300 text-center">
+          HOVER ON CARDS
         </h1>
         <div className="flex gap-5 justify-center items-center pt-10 flex-wrap">
           {CARD_ANIMATION_DATA_LIST.map((obj, i) => (
             <div key={i}>
-              <div className="relative overflow-hidden rounded-xl w-full">
+              <div className="relative overflow-hidden rounded-xl w-full border hover:border-blue-500">
                 {/* Outer Image */}
                 <img
                   className="max-w-[429px] max-sm:max-w-[300px] lg:min-h-[357px] w-full bg-white object-cover relative z-40 hover:z-0 duration-500 hover:opacity-0 cursor-pointer"
@@ -21,7 +21,7 @@ const KeyframeMovement = () => {
                   alt="outerImg"
                 />
                 {/* Continuous Scrolling Images */}
-                <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+                <div className="absolute top-0 left-0 w-full h-full overflow-hidden shadow-2xl">
                   <div className="image-slider">
                     {obj.images.concat(obj.images).map((image, index) => (
                       <img
